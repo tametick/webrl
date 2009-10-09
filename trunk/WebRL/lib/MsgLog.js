48ed53f749892ed5cddeed0cdf1250376227aaf7
michael.duerwald <michael.duerwald@1a3de590-b4ac-11de-880b-11b4e8d67c75>
2009-10-09 21:25:07 +0000

    changed variable style naming; added logbuffer

--- trunk/WebRL/lib/MsgLog.js
+++ trunk/WebRL/lib/MsgLog.js
@@ -1,14 +1,14 @@
 function MsgLog(){
-    this._logBuffer = 100;
-    this._visibleLogBuffer = 5;
-    this._data = new Array();
+    this.logBuffer = 100;
+    this.visibleLogBuffer = 5;
+    this.data = new Array();
 }
 
 /* a very generic method to append data to a log */
 MsgLog.prototype.append = function(toAdd){
-    this._data.push(toAdd);
-    if (this._data.length > 5) {
-        this._data.shift();
+    this.data.push(toAdd);
+    if (this.data.length > this.logBuffer) {
+        this.data.shift();
     }
     
 }
@@ -21,14 +21,14 @@ MsgLog.prototype.renderToHtml = function(){
     while (log.hasChildNodes()) 
         log.removeChild(log.childNodes[0]);
     
-    var max = this._data.length - 1;
-    var min = max - this._visibleLogBuffer;
+    var max = this.data.length - 1;
+    var min = max - this.visibleLogBuffer;
 	if (min < 0)
 		min = 0;
 		    
     for (var i = max; i >= min; i--) {
         var newItem = document.createElement("p");
-        var newText = document.createTextNode(this._data[i]);
+        var newText = document.createTextNode(this.data[i]);
         newItem.appendChild(newText);
         log.appendChild(newItem);
     }

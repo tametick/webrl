06eed90008d8f168430f92b57a47ed22086c736a
Ido.Yehieli <Ido.Yehieli@1a3de590-b4ac-11de-880b-11b4e8d67c75>
2009-10-09 08:36:35 +0000

    seperated css & js to their own files

---
+++ trunk/WebRL/WebRL.js
@@ -0,0 +1,74 @@
+var playerx = 0;
+var playery = 0;
+
+function main(){
+    Screen(80, 24);
+    
+    for (var y = 0; y < 24; y++)
+        for (var x = 0; x < 80; x++)
+            Screen.addColoredChar(x, y, new ColoredChar('.', "red"));
+    
+    Screen.update();
+}
+
+document.onkeypress = function(e){
+    var e = window.event || e;
+    
+    Screen.addColoredChar(playerx, playery, new ColoredChar('.', "red"));
+    
+    if (e.keyCode == 37) // Left
+        playerx--;
+    else if (e.keyCode == 38) // Up
+        playery--;
+    else if (e.keyCode == 39) // Right
+        playerx++;
+    else if (e.keyCode == 40) // Down
+        playery++;
+    
+    Screen.addColoredChar(playerx, playery, new ColoredChar('@', "blue"));
+    Screen.update();
+}
+
+function ColoredChar(ch, color){
+    this.ch = ch;
+    this.color = color;
+}
+
+ColoredChar.prototype.toString = function(){
+    var s = "<font color=\"" + this.color + "\">" + this.ch + "</font>";
+    return s;
+}
+
+ColoredChar.prototype.fromString = function(){
+
+}
+
+function Screen(width, height){
+    Screen.width = width;
+    Screen.height = height;
+    
+    // This will hold all the updated cells.
+    Screen.data = new Array();
+    
+    var s = '<table class="display" >';
+    for (var y = 0; y < height; y++) {
+        s += '<tr class="display">';
+        for (var x = 0; x < width; x++)
+            s += '<td class="display" id="tile' + x + "," + y + '"></td>';
+        s += '</tr>';
+    }
+    s += '</table>';
+    
+    document.getElementById("screen").innerHTML = s;
+}
+
+Screen.addColoredChar = function(x, y, cc){
+    Screen.data["" + x + "," + y] = cc;
+}
+
+Screen.update = function(){
+    for (var i in Screen.data)
+        document.getElementById("tile" + i).innerHTML = Screen.data[i];
+
+    Screen.data = new Array();
+}
